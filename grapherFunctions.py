a22dd4353b7350cd873adc86d3d3c48e644012a4
JusticeSwann <90804100+JusticeSwann@users.noreply.github.com>
2024-02-19 16:23:04 -0400

    Added Gridlines to graphs, added functionality for best fit line

--- grapherFunctions.py
+++ grapherFunctions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def createDataFrame(path_to_data):
@@ -7,8 +8,22 @@ def createDataFrame(path_to_data):
     return df
 
 
-def generateScatterPlot(title, x ,y ,xAxisLabel, yAxisLabel):
-    plt.plot(x, y)
+
+def generateScatterPlot(title, x ,y ,xAxisLabel, yAxisLabel,bestfit=False):
+    plt.scatter(x, y)
+    
+    # Add gridlines
+    plt.grid(True)
+    
+    if bestfit == True:
+        # Calculate and plot best fit line
+        coeffs = np.polyfit(x, y, 1)
+        best_fit = np.poly1d(coeffs)
+        plt.plot(x, best_fit(x), color='red')
+    else:
+        plt.plot(x, y)
+    
+    
     plt.xlabel(xAxisLabel)
     plt.ylabel(yAxisLabel)
     plt.title(title)
